15b23b10b9869a089006dcd9632672e35e81ec93
JAY-PRAJAPATI-4216 <jayuprajapati4216@gmail.com>
2026-08-18 13:54:56 +0530

    dynamic memory programe

---
+++ pointer/dynamicallocation.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p,n,*temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    p = (int *)malloc(sizeof(int)*n);
+    
+    if (p == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    temp = p;
+    for(int i = 0; i<n; i++) {
+        printf("%d \n", temp);
+        temp ++;
+    }
+    temp = p;
+    for (int i = 0; i < n; i++) {
+        printf("Enter a value for index  %d: ", i);
+        scanf("%d", (temp + i));
+    }
+    temp = p;
+    for (int i = 0; i < n; i++) {
+        printf("Value at index %d: %d\n", i, *temp);
+        temp++;
+    }
+    
+    return 0;
+}
+
